d1d8349a49828a61c29d15398f5ff602c8b35573
Tera-Si <cheuklunterence@gmail.com>
2021-08-23 11:36:06 +0800

    post item (but not images) now requires a valid token

--- controllers/items.js
+++ controllers/items.js
@@ -1,5 +1,6 @@
 const itemsRouter = require("express").Router()
 const multer = require("multer")
+const userExtractor = require("../utils/middlewares").userExtractor
 
 const storageOptions = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -21,9 +22,17 @@ const upload = multer({
   }
 }).array("item-images", 8)
 
-itemsRouter.post("/", upload, async (request, response) => {
+// TODO: handle post errors
+// TODO: Item schema, and post item to MongoDB
+// TODO: route for GET items and individual item
+// TODO: change it so that no images will be uploaded if no token were provided
+itemsRouter.post("/", [upload, userExtractor], async (request, response) => {
   // TODO: remove this console.log
   // console.log(request.body["item-name"])
+
+  if (!request.user) {
+    return response.status(401).json({ error: "missing or invalid token" })
+  }
 
   return response.status(201).send(request.file)
 })
