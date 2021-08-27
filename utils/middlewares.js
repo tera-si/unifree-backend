8650227e411294405160ab104ea8eca16f2ad94d
Tera-Si <cheuklunterence@gmail.com>
2021-08-27 15:54:56 +0800

    implemented error handlers for invalid token and file uploads

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -18,6 +18,12 @@ const errorHandler = (error, request, response, next) => {
   else if (error.name === "CastError") {
     return response.status(400).json({ error: "unrecognized ID" })
   }
+  else if (error.message === "Only jpg, jpeg, png, and gif files are accepted") {
+    return response.status(400).json({ error: "Only jpg, jpeg, png, and gif files are accepted" })
+  }
+  else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "invalid token" })
+  }
 
   next(error)
 }
