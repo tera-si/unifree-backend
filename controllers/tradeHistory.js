dcb5ba1bded01f421075eb1e29ea547f5f5c75cc
tera-si <cheuklunterence@gmail.com>
2021-10-26 02:45:30 +0800

    implemented get /api/tradehistory

--- controllers/tradeHistory.js
+++ controllers/tradeHistory.js
@@ -37,4 +37,49 @@ tradeHistoryRouter.post("/", userExtractor, async (request, response) => {
   return response.status(201).json(result)
 })
 
+tradeHistoryRouter.get("/", userExtractor, async (request, response) => {
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: "missing or invalid token" })
+  }
+
+  const ownedByUser = await TradeHistory.find({ itemOwner: user._id })
+    .populate("itemOwner", { username: 1, _id: 1 })
+    .populate("tradedWith", { username: 1, _id: 1 })
+    .populate("item", { name: 1, _id: 1 })
+  const tradedWithUser = await TradeHistory.find({ tradedWith: user._id })
+    .populate("itemOwner", { username: 1, _id: 1 })
+    .populate("tradedWith", { username: 1, _id: 1 })
+    .populate("item", { name: 1, _id: 1 })
+
+  const allEntries = [].concat(ownedByUser).concat(tradedWithUser)
+
+  return response.status(200).json(allEntries)
+})
+
+tradeHistoryRouter.get("/:id", userExtractor, async (request, response) => {
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: "missing or invalid token" })
+  }
+
+  const matchedEntry = await TradeHistory.findById(request.params.id)
+    .populate("itemOwner", { username: 1, _id: 1 })
+    .populate("tradedWith", { username: 1, _id: 1 })
+    .populate("item", { name: 1, _id: 1 })
+
+  if (!matchedEntry) {
+    return response.status(404).json({ error: "no such entry" })
+  }
+
+  if (
+    JSON.stringify(user._id) !== JSON.stringify(matchedEntry.itemOwner._id) &&
+    JSON.stringify(user._id) !== JSON.stringify(matchedEntry.tradedWith._id)
+  ) {
+    return response.status(403).json({ error: "not authorized for this action" })
+  }
+
+  return response.status(200).json(matchedEntry)
+})
+
 module.exports = tradeHistoryRouter
