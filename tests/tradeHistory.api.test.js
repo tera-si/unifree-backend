4c66d12f406f84eff1d9ab43b4528cd24a80cd9c
tera-si <cheuklunterence@gmail.com>
2021-10-27 22:18:05 +0800

    implemented tests for POST /api/tradehistory

--- tests/tradeHistory.api.test.js
+++ tests/tradeHistory.api.test.js
@@ -16,6 +16,25 @@ let tokens = []
 let userIDs = []
 let itemIDs = []
 
+const thirdUser = {
+  username: "third_username",
+  password: "third_password",
+  items: []
+}
+const thirdItem = {
+  name: "Beige Fedora",
+  category: "Fashion accessory",
+  condition: "New",
+  shipping: true,
+  meet: true,
+  description: "Brand new never wore. Beige Fedora",
+  imagePaths: [
+    "fedora-pexels-rachel-claire-6123270.jpg"
+  ],
+  datePosted: new Date(),
+  availability: true
+}
+
 beforeAll(async () => {
   await User.deleteMany({})
   await Item.deleteMany({})
@@ -28,14 +47,8 @@ beforeAll(async () => {
     userIDs.push(response.body.id)
   }
 
-  const thirdUser = {
-    username: "third_username",
-    password: "third_password",
-    items: []
-  }
-
   await api.post("/api/users").send(thirdUser)
-  const response = await api.post("/api/login").send(thirdUser)
+  let response = await api.post("/api/login").send(thirdUser)
   tokens.push(response.body.token)
   userIDs.push(response.body.id)
 
@@ -55,6 +68,19 @@ beforeAll(async () => {
     itemIDs.push(response.body.id)
     i++
   }
+
+  response = await api
+    .post("/api/items")
+    .set("Authorization", `bearer ${tokens[2]}`)
+    .field("item-name", thirdItem.name)
+    .field("item-category", thirdItem.category)
+    .field("item-condition", thirdItem.condition)
+    .field("item-shipping", thirdItem.shipping)
+    .field("item-meet", thirdItem.meet)
+    .field("item-description", thirdItem.description)
+    .attach("item-images", `tests/images/post_items/${thirdItem.imagePaths[0]}`)
+
+  itemIDs.push(response.body.id)
 }, 20000)
 
 beforeEach(async () => {
@@ -267,6 +293,241 @@ describe("GET from /api/tradehistory/:id", () => {
       .get(`/api/tradehistory/${inDB[0]._id}`)
       .set("Authorization", `${tokens[0]}`)
       .expect(401)
+  })
+})
+
+describe("POST to /api/tradehistory", () => {
+  test("successfully added a valid entry with valid and matching token", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: userIDs[0]
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+      .expect(201)
+      .expect("Content-Type", /application\/json/)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2 + 1)
+  })
+
+  test("new entry is correctly stored in database", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: userIDs[0]
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+
+    const result = await TradeHistory.find({ itemOwner: userIDs[2] })
+    expect(result).toHaveLength(1)
+    expect(result[0].dateDelisted).toBeDefined()
+    expect(result[0]._id).toBeDefined()
+
+    const receivedObj = { ...JSON.parse(JSON.stringify(result[0])) }
+    delete receivedObj.id
+    delete receivedObj.dateDelisted
+
+    const expectedObj = { ...newEntry }
+    expectedObj.itemOwner = userIDs[2]
+
+    expect(receivedObj).toEqual(expectedObj)
+  })
+
+  test("reject request with invalid user selected", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: -1
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+      .expect(400)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request with non existent traded with user", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: await userHelper.nonExistentUserID()
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+      .expect(404)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request where itemOwner is same as tradedWWith user", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: userIDs[2]
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+      .expect(400)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request with non existent item", async () => {
+    const newEntry = {
+      item: await itemHelper.nonExistentItemID(),
+      tradedWith: userIDs[0]
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+      .expect(404)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request if the item's availability is false", async () => {
+    const updatedObject = {
+      ...thirdItem,
+      availability: false
+    }
+
+    await api
+      .put(`/api/items/${itemIDs[2]}`)
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(updatedObject)
+      .expect(200)
+
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: userIDs[0]
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+      .expect(400)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request to mark item not owned by user as traded", async () => {
+    const newEntry = {
+      item: itemIDs[0],
+      tradedWith: userIDs[0],
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(newEntry)
+      .expect(403)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request with missing fields", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: userIDs[0]
+    }
+
+    const noItem = { ...newEntry }
+    delete noItem.item
+
+    const noUser = { ...newEntry }
+    delete noUser.tradedWith
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(noItem)
+      .expect(400)
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2]}`)
+      .send(noUser)
+      .expect(400)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request without token", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: userIDs[0],
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .send(newEntry)
+      .expect(401)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
+  })
+
+  test("reject request with invalid tokens", async () => {
+    const newEntry = {
+      item: itemIDs[2],
+      tradedWith: userIDs[0]
+    }
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer ${tokens[2].substring(4)}`)
+      .send(newEntry)
+      .expect(401)
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer abc123`)
+      .send(newEntry)
+      .expect(401)
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `bearer @@@`)
+      .send(newEntry)
+      .expect(401)
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `basic ${tokens[0]}`)
+      .send(newEntry)
+      .expect(401)
+
+    await api
+      .post("/api/tradehistory")
+      .set("Authorization", `${tokens[0]}`)
+      .send(newEntry)
+      .expect(401)
+
+    const result = await TradeHistory.find({})
+    expect(result).toHaveLength(2)
   })
 })
 
